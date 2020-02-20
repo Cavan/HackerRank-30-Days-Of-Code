5246e69f473b844fa294162c56b80d38b63be446
Cavan Biggs <cbiggs7635@conestogac.on.ca>
2020-02-20 15:57:44 -0500

    Completed Day 1 challenge

---
+++ ThirtyDaysOfCode/main.cpp
@@ -0,0 +1,19 @@
+//Run all solutions from main
+//Created by Cavan Biggs on February 19th 2020
+
+
+#include "Day_1_Challenge.h";
+
+
+int main()
+{
+	//Run Day 1 solution
+	Day_1_Challenge();
+	//Run Day 2 solution
+
+	//Run Day 3 solution
+
+	//Run day 4 solution
+
+	//Run day 5 solution
+}
